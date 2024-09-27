c2e3dae5724d9317f3a3f49ee78318e60b5186b7
Mykhailo Holub <mishataube2005@gmail.com>
2024-09-27 14:03:44 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,5 +1,4 @@
 import pandas
-import PythonTableConsole as PTC
 import matplotlib.pyplot as plt
 
 import Corelation
@@ -9,42 +8,35 @@ df_raw = pandas.read_csv('data/saveecobot_16181.csv', on_bad_lines='warn')  # op
 
 print(df_raw.head())
 
-print("Raw table height:", df_raw.size)
+print("Raw table height:", df_raw.shape[0])
 
 
 df_raw = df_raw.dropna(axis=0, subset='value')  # drop all NAN values in column 'values'
-df_raw = df_raw.drop("value_text", axis = 1)
+df_raw = df_raw.drop(["value_text",'device_id'], axis = 1)
 
-print("Table height after dropping nan values:", df_raw.size)
+print("Table height after dropping nan values:", df_raw.shape[0])
 
 print('Group by phenomenon to get all phenomenon names:')
 print(pandas.DataFrame(df_raw.groupby('phenomenon')))
 print()
 
 df_pm1 = df_raw.loc[df_raw['phenomenon'] == 'pm1'].set_index("logged_at")
-df_pm1 = df_pm1.rename(columns={'value': 'pm_1'}).drop(['device_id','phenomenon'], axis=1)
+df_pm1 = df_pm1.rename(columns={'value': 'pm_1'}).drop('phenomenon', axis=1)
 
 df_pm25 = df_raw.loc[df_raw['phenomenon'] == 'pm25'].set_index("logged_at")
-df_pm25 = df_pm25.rename(columns={'value': 'pm_25'}).drop(['device_id','phenomenon'], axis=1)
+df_pm25 = df_pm25.rename(columns={'value': 'pm_25'}).drop('phenomenon', axis=1)
 
 df_pm10 = df_raw.loc[df_raw['phenomenon'] == 'pm10'].set_index("logged_at")
-df_pm10 = df_pm10.rename(columns={'value': 'pm_10'}).drop(['device_id','phenomenon'], axis=1)
+df_pm10 = df_pm10.rename(columns={'value': 'pm_10'}).drop('phenomenon', axis=1)
 
 df_pressure_pa = df_raw.loc[df_raw['phenomenon'] == 'pressure_pa'].set_index("logged_at")
-df_pressure_pa = df_pressure_pa.rename(columns={'value': 'pressure_pa'}).drop(['device_id','phenomenon'], axis=1)
+df_pressure_pa = df_pressure_pa.rename(columns={'value': 'pressure_pa'}).drop('phenomenon', axis=1)
 
 df_temperature = df_raw.loc[df_raw['phenomenon'] == 'temperature'].set_index("logged_at")
-df_temperature = df_temperature.rename(columns={'value': 'temperature'}).drop(['device_id','phenomenon'], axis=1)
+df_temperature = df_temperature.rename(columns={'value': 'temperature'}).drop('phenomenon', axis=1)
 
 df_humidity = df_raw.loc[df_raw['phenomenon'] == 'humidity'].set_index("logged_at")
-df_humidity = df_humidity.rename(columns={'value': 'humidity'}).drop(['device_id','phenomenon'], axis=1)
-
-list_length = [['phenomenon', 'pm1', 'pm10', 'pm25', 'humidity', 'pressure_pa', 'temperature'],
-               ['size', df_pm1.size, df_pm25.size, df_pm10.size, df_humidity.size, df_pressure_pa.size,
-                df_temperature.size]]
-
-table_length = PTC.PythonTableConsole(list_length)
-print(table_length)
+df_humidity = df_humidity.rename(columns={'value': 'humidity'}).drop('phenomenon', axis=1)
 
 # merge all sub-dfs in one df
 df_cooked = pandas.merge(df_pm1, df_pm10,on="logged_at")
@@ -102,15 +94,15 @@ df_test = df_test.set_index("Date")
 print(df_train.size,df_test.size)
 
 
-model = RModel.LReg(df_train,df_test,["pm_1"], "pm_25")
+model = RModel.LReg(df_train,df_test,["pm_25", "pm_1", "pm_10", "temperature", "Hour", "pressure_pa"], "humidity")
 print(model.test_r_sq(),model.test_rmse())
 
 prediction = model.predict()
 
 plt.figure(figsize=(12, 6))
-plt.plot(df_train.index, df_train["pm_25"].rolling(window=10000).mean(), label='Training Data', color='blue')
-plt.plot(df_test.index, df_test['pm_25'].rolling(window=10000).mean(), label='Testing Data', color='green')
-plt.plot(prediction.index, prediction["Prediction"].rolling(window=10000).mean(), color='red', label='Predicted ppm 2.5')
+plt.plot(df_train.index, df_train["humidity"].rolling(window=10000).mean(), label='Training Data', color='blue')
+plt.plot(df_test.index, df_test['humidity'].rolling(window=10000).mean(), label='Testing Data', color='green')
+plt.plot(prediction.index, prediction["Prediction"].rolling(window=10000).mean(), color='red', label='Predicted humidity')
 plt.xlabel('Timestamp')
 plt.ylabel('Value')
 plt.legend()
