9e6dab8755af0a524139b6fec09d86119f42232b
Mykhailo Holub <mishataube2005@gmail.com>
2024-10-24 21:38:21 +0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- lab_2/Question.py
+++ lab_2/Question.py
@@ -1,5 +1,5 @@
 import copy
-
+import numpy
 
 def get_sub(x):
     normal = "0123456789+-=()"
@@ -8,13 +8,12 @@ def get_sub(x):
     return x.translate(res)
 
 class Question:
-    def __init__(self, find_max: bool, main_func: list, constrains: list, constrains_op: list):
-        if not find_max:
+    def __init__(self, find_max: bool, main_func: list, constrains: list):
+        if find_max:
             self.main_func = main_func
         else:
             self.main_func = [-i for i in main_func]
         self.constrains = constrains
-        self.original_length = len(main_func)
 
     def print_question(self):
         line = ""
@@ -23,7 +22,7 @@ class Question:
                 line += "+ " + str(self.main_func[i])+"*X"+get_sub(str(i+1))+" "
             elif self.main_func[i] < 0:
                 line += "- "+str(abs(self.main_func[i]))+"*X"+get_sub(str(i+1))+" "
-        line += "--> min"
+        line += "--> max"
         print(line[1:])
         for constraint in self.constrains:
             line = ""
@@ -34,33 +33,24 @@ class Question:
                     line += "- " + str(abs(constraint[i])) + "*X" + get_sub(str(i + 1)) + " "
             print(line[1:] + str(constraint[-2])+" "+str(constraint[-1]))
 
-    def prepare(self):
-        # Вільний доданок має бути невід'ємним
+    def solve(self):
+        full_list = [[0]+[-i for i in self.main_func]+[0]*(len(self.constrains)+2)]
+
         for i in range(len(self.constrains)):
-            if self.constrains[i][-1]<0:
-                self.constrains[i] = [-a for a in self.constrains[i]]
+            constraint = [0]+self.constrains[i][:-2] + [0]*(len(self.constrains)+2) + [self.constrains[i][-1]]
+            constraint[i+len(self.main_func)+1] = 1
+            if self.constrains[-2] == "<=":
+                constraint[-2] = 1
+            else:
+                constraint[-2] = -1
+            full_list += [constraint]
+        print(full_list)
 
-        # Зведення нерівностей з <=
-        for i in range(len(self.constrains)):
-            if self.constrains[i][-2] == "<=":
-                self.main_func.append(0)
-                for j in range(len(self.constrains)):
-                    self.constrains[j].insert(-2, 0)
-                self.constrains[-3] = 1
-                self.constrains[i][-2] = "="
-                self.marked[i] = True
-
-        # Зведення нерівностей з >=
-        for i in range(len(self.constrains)):
-            if self.constrains[i][-2] == "<=":
-                self.main_func.append(0)
-                for j in range(len(self.constrains)):
-                    self.constrains[j].insert(-2, 0)
-                self.constrains[-3] = -1
-                self.constrains[i][-2] = "="
-                self.marked[i] = True
-
-        #
-
+        full_list = numpy.array(full_list)
+        while full_list[0, 1:full_list.shape[1]-2].min() < 0:
+            pivot_column = full_list[:, full_list[:, ].argmin()]
+            for i in range(len(self.constrains)):
+                full_list[i+1][-1] = full_list[i+1][-2] / pivot_column[i+1]
+            ratios = full_list[1:][-1]
 
 
--- lab_2/main.py
+++ lab_2/main.py
@@ -7,6 +7,7 @@ var_3 = Question(True,
                  [[2, -1, 0, -2, 1, 0, "=", 16,],
                   [3, 2, 1, -3, 0, 0, "=", 18],
                   [-1, 3, 0, 4, 0, 1, "=", 24]])
+
 var_3.print_question()
 print()
-var_3.print_question()
+var_3.solve()
